51d88eb1f2e33e74876e2a6804146d4a0deb98da
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-10-14 12:15:41 +0300

    added existed project

---
+++ project/bookstore/src/main/java/ecommerce/project/bookstore/security/Authority.java
@@ -0,0 +1,17 @@
+package ecommerce.project.bookstore.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+
+	private final String authority;
+
+	public Authority(String authority) {
+		this.authority = authority;
+	}
+
+	@Override
+	public String getAuthority() {
+		return authority;
+	}
+}
